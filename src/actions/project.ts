deae39a8f0a5ec9b44a310e38cd68aaf877fe77c
Himanish kochhar <himanish.kochhar@gmail.com>
2024-12-16 15:48:54 +0530

    action project file for initial step

---
+++ src/actions/project.ts
@@ -0,0 +1,13 @@
+'use server'
+import z from 'zod'
+import { projectSchema } from '@/schemas/project'
+
+export async function createProject(unsafeData: z.infer<typeof projectSchema>) {
+  const data = projectSchema.safeParse(unsafeData)
+
+  if (!data.success) return { success: false }
+
+  // Save to DB
+
+  return { success: true }
+}
