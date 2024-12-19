baea15e0199db35a991abe5b8d30236cfe956c07
Himanish kochhar <himanish.kochhar@gmail.com>
2024-12-20 04:23:23 +0530

    added the form field and controller and added the description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 "use client"
 
-import { useForm } from "react-hook-form"
+import { Controller, useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { projectSchema } from '@/schemas/project'
 import z from "zod"
 import { createProject } from "@/actions/project"
 import { toast } from "sonner"
 import { Divide } from "lucide-react"
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 export default function Home() {
 
-  const form = useForm<z.infer<typeof projectSchema>>({
+  const form = useForm({
     defaultValues: {
-      name: ""
+      name: "",
+      description: ""
     },
     resolver: zodResolver(projectSchema)
 
@@ -31,9 +35,21 @@ export default function Home() {
   }
 
   return (
-    <div className="container px-4 mx-auto my-6">
+    <div className="container px-4 mx-auto my-6 ">
       <form onSubmit={form.handleSubmit(onSubmit)}>
-
+        <FieldGroup>
+          <Controller
+            control={form.control}
+            name="name" render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor={field.name}>Name</FieldLabel>
+                <Input {...field} id={field.name} aria-invalid={fieldState.invalid} />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>)} />
+          <Button>Create</Button>
+        </FieldGroup>
       </form>
     </div>
   )
