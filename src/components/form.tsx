c7230a6774f577dda76cad260016885835874e13
Himanish kochhar <himanish.kochhar@gmail.com>
2024-12-29 11:10:06 +0530

    form base and typesafety added

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -9,18 +9,34 @@ type FormControlProps<TFieldValues extends FieldValues = FieldValues, TName exte
     control: ControllerProps<TFieldValues, TName, TTransformedValues>["control"]
 }
 
-export function FormInput<TFieldValues extends FieldValues = FieldValues, TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>, TTransformedValues = TFieldValues>({ control, name, label }: FormControlProps<TFieldValues, TName, TTransformedValues>) {
-
+type FormBaseProps<TFieldValues extends FieldValues = FieldValues, TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>, TTransformedValues = TFieldValues> =
+    FormControlProps<TFieldValues, TName, TTransformedValues> & {
+        children: (field: Parameters<ControllerProps<TFieldValues, TName, TTransformedValues>["render"]>[0]["field"] & {
+            "aria-invalid": boolean,
+            id: string
+        }) => ReactNode
+    }
+function FormBase<TFieldValues extends FieldValues = FieldValues, TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>, TTransformedValues = TFieldValues>({ children, control, label, name }: FormBaseProps<TFieldValues, TName, TTransformedValues>) {
     return (
         <Controller
             control={control}
             name={name} render={({ field, fieldState }) => (
                 <Field data-invalid={fieldState.invalid}>
                     <FieldLabel htmlFor={field.name}>{label}</FieldLabel>
-                    <Input {...field} id={field.name} aria-invalid={fieldState.invalid} />
+                    {children({ ...field, id: field.name, "aria-invalid": fieldState.invalid })}
                     {fieldState.invalid && (
                         <FieldError errors={[fieldState.error]} />
                     )}
                 </Field>)} />
+    )
+}
+
+
+export function FormInput<TFieldValues extends FieldValues = FieldValues, TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>, TTransformedValues = TFieldValues>(props: FormControlProps<TFieldValues, TName, TTransformedValues>) {
+
+    return (
+        <FormBase {...props}>
+            {field => <Input {...field} />}
+        </FormBase>
     )
 }
