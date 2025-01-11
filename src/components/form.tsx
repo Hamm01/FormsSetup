03266abb9780010481dc718038986fce946fa0ae
Himanish kochhar <himanish.kochhar@gmail.com>
2025-01-11 18:09:39 +0530

    FormSelect added and formcontrolfunc type update with childeren prop

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -21,7 +21,7 @@ type FormBaseProps<TFieldValues extends FieldValues = FieldValues, TName extends
         }) => ReactNode
     }
 
-type FormControlFunc = <TFieldValues extends FieldValues = FieldValues, TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>, TTransformedValues = TFieldValues>(props: FormControlProps<TFieldValues, TName, TTransformedValues>) => ReactNode
+type FormControlFunc<ExtraProps extends Record<string, unknown> = Record<never, never>> = <TFieldValues extends FieldValues = FieldValues, TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>, TTransformedValues = TFieldValues>(props: FormControlProps<TFieldValues, TName, TTransformedValues> & ExtraProps) => ReactNode
 
 function FormBase<TFieldValues extends FieldValues = FieldValues, TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>, TTransformedValues = TFieldValues>({ children, control, label, name, description }: FormBaseProps<TFieldValues, TName, TTransformedValues>) {
     return (
@@ -56,4 +56,23 @@ export const FormTextArea: FormControlFunc = (props) => {
 }
 
 
+export const FormSelect: FormControlFunc<{ children: ReactNode }> = ({
+    children,
+    ...props
+}) => {
+    return <FormBase {...props}>
+        {({ onChange, onBlur, ...field }) => (
+
+            <Select {...field} onValueChange={onChange}>
+                <SelectTrigger aria-invalid={field["aria-invalid"]} id={field.id} onBlur={onBlur}>
+                    <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                    {children}
+                </SelectContent>
+            </Select>
+        )
+        }
+    </FormBase>
+}
 
