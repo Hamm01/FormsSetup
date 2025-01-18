dad96d17903c532dd64cf0bc1c4239739c6c79fd
Himanish kochhar <himanish.kochhar@gmail.com>
2025-01-18 13:13:28 +0530

    replace the field code with field.input in main form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,16 +62,7 @@ export default function Home() {
       }}>
         <FieldGroup>
           <form.AppField name='name'>
-            {field => {
-              <field.Input />
-              const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid
-              return <Field data-invalid={isInvalid}>
-                <FieldLabel htmlFor={field.name}>Name</FieldLabel>
-                <Input id={field.name} name={field.name} value={field.state.value} onBlur={field.handleBlur} onChange={e => field.handleChange(e.target.value)} aria-invalid={isInvalid} />
-                {isInvalid && <FieldError errors={field.state.meta.errors} />}
-
-              </Field>
-            }}
+            {field => <field.Input />}
           </form.AppField>
           {/* <FormSelect control={form.control}
             name="status" label="status">
