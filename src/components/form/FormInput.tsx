414dd400e7c8776931cea104463116d951a3ca18
Himanish kochhar <himanish.kochhar@gmail.com>
2025-01-19 09:40:34 +0530

    FormBase added in form input

--- src/components/form/FormInput.tsx
+++ src/components/form/FormInput.tsx
@@ -1,17 +1,20 @@
-import { Field, FieldError, FieldLabel } from "../ui/field"
 import { Input } from "../ui/input"
+import { FormBase, FormControlProps } from "./Formbase"
 import { useFieldContext } from "./hooks"
 
 
-export function FormInput() {
+export function FormInput(props: FormControlProps) {
     const field = useFieldContext<string>()
     const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid
-    return (<Field data-invalid={isInvalid}>
-
-        <FieldLabel htmlFor={field.name}>Name</FieldLabel>
-        <Input id={field.name} name={field.name} value={field.state.value} onBlur={field.handleBlur} onChange={e => field.handleChange(e.target.value)} aria-invalid={isInvalid} />
-        {isInvalid && <FieldError errors={field.state.meta.errors} />}
-
-    </Field>)
-
+    return (
+        <FormBase {...props}>
+            <Input
+                id={field.name}
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={e => field.handleChange(e.target.value)}
+                aria-invalid={isInvalid} />
+        </FormBase>
+    )
 }
