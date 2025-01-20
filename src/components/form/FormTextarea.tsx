9a4d9a6149bdd8fdb17276aa02aae4cf46ab06d1
Himanish kochhar <himanish.kochhar@gmail.com>
2025-01-20 11:56:13 +0530

    FormTextarea file added which has form base component as common in forminput component

---
+++ src/components/form/FormTextarea.tsx
@@ -0,0 +1,20 @@
+import { Textarea } from "../ui/textarea"
+import { FormBase, FormControlProps } from "./Formbase"
+import { useFieldContext } from "./hooks"
+
+
+export function FormTextarea(props: FormControlProps) {
+    const field = useFieldContext<string>()
+    const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid
+    return (
+        <FormBase {...props}>
+            <Textarea
+                id={field.name}
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={e => field.handleChange(e.target.value)}
+                aria-invalid={isInvalid} />
+        </FormBase>
+    )
+}
